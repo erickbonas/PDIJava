418cf4bb4ed9dcf276477afc8ec089d9d60f7d06
Erick Bonni <motorhead01@hotmail.com>
2013-05-08 19:26:37 -0300

    Deletado SecondActivuty e todas as suas referencias em outros arquivos

--- gen/com/example/pdiaproject/R.java
+++ gen/com/example/pdiaproject/R.java
@@ -25,12 +25,10 @@ public final class R {
         public static final int papaco_logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int main_button=0x7f080000;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int second=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -38,8 +36,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int image_name=0x7f050003;
-        public static final int next_screen=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/example/pdiaproject/MainActivity.java
+++ src/com/example/pdiaproject/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.pdiaproject;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+
 
 
 public class MainActivity extends Activity 
@@ -17,15 +14,6 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		Button button = (Button) findViewById(R.id.main_button);
-		
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent( MainActivity.this, SecondActivity.class);
-				startActivity(intent);
-			}
-		});
 	}
 
 	@Override
